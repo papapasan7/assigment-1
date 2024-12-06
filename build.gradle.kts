0ec2ff32693912f7b650730fcbdb093ad44497e2
papapasan7 <a.filkach@gmail.com>
2024-12-06 13:31:29 +0000

    add jar fat

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,22 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
     application
 }
-
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    // for building a fat jar - include all dependencies
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+}
 group = "ie.setu"
-version = "1.0-SNAPSHOT"
+version = "4.0"
 
 repositories {
     mavenCentral()
